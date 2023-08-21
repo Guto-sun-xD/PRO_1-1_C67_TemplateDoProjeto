b23db84428c0eeb4c52eafc32e708815c2aaa642
Guto <96207727+Guto-sun-xD@users.noreply.github.com>
2023-08-21 17:13:39 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDhQeG5s0SyYrlpLAyymZJPbIFH69RdXYU",
+  authDomain: "projeto67-1af98.firebaseapp.com",
+  projectId: "projeto67-1af98",
+  storageBucket: "projeto67-1af98.appspot.com",
+  messagingSenderId: "130258196156",
+  appId: "1:130258196156:web:86661c09018c24921b22b9"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
